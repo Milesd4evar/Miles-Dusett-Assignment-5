bafd3ad00bfc522d3f11abea60dd7ad56917589c
Milesd4evar <milesd8777420@gmail.com>
2026-03-16 10:06:02 -0700

    fixing error with wrong file uploaded

--- Query3.js
+++ Query3.js
@@ -5,17 +5,12 @@ async function main() {
   try {
     await client.connect();
     const db = client.db('ieeevisTweets');
-    const results = await db.collection('tweet').aggregate([
-      { $group: { 
-          _id: "$user.screen_name", 
-          tweetCount: { $sum: 1 }, 
-          avgRetweets: { $avg: "$retweet_count" } 
-      } },
-      { $match: { tweetCount: { $gt: 3 } } },
-      { $sort: { avgRetweets: -1 } },
-      { $limit: 10 }
+    const result = await db.collection('tweet').aggregate([
+      { $group: { _id: "$user.screen_name", tweetCount: { $sum: 1 } } },
+      { $sort: { tweetCount: -1 } },
+      { $limit: 1 }
     ]).toArray();
-    console.table(results);
+    console.log(result[0]);
   } finally {
     await client.close();
   }
